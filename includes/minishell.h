97977fbb7ce8356c6541716506c8ad1ac1f30e99
Bernardo Conejo <bconejo-@student.42malaga.com>
2026-05-28 20:26:15 +0200

    refactor: Reorganize includes and structure definitions in minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,31 +1,57 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <readline/readline.h>
 # include <readline/history.h>
-# include <unistd.h>	// write, read, access, close, fork, execve, dup, dup2,
-			// pipe, isatty, ttyname, ttyslot, getcwd, chdir, unlink
-# include <fcntl.h>	// open
-# include <stdio.h>	// printf, perror
-# include <stdlib.h>	// malloc, free, getenv, exit
-# include <string.h>	// strerror
-# include <stdio.h>	// printf, perror
-# include <stdlib.h>	// malloc, free, getenv, exit
-# include <string.h>	// strerror
-# include <sys/wait.h>	// wait, waitpid, wait3, wait4
-# include <signal.h>	// signal, sigaction, sigemptyset, sigaddset, kill
-# include <sys/stat.h>	// stat, lstat, fstat
-# include <dirent.h>	// opendir, readdir, closedir
-# include <termios.h>	// tcsetattr, tcgetattr
-# include <sys/ioctl.h>	// ioctl
-# include <term.h>	// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
-
+# include <readline/readline.h>
+# include <unistd.h> // write, read, access, close, fork, execve, dup, dup2,
+					// pipe, isatty, ttyname, ttyslot, getcwd, chdir, unlink
 # include "libft.h"
+# include <dirent.h> // opendir, readdir, closedir
+# include <fcntl.h>  // open
 # include <limits.h>
+# include <signal.h>    // signal, sigaction, sigemptyset, sigaddset, kill
+# include <stdio.h>     // printf, perror
+# include <stdlib.h>    // malloc, free, getenv, exit
+# include <string.h>    // strerror
+# include <sys/ioctl.h> // ioctl
+# include <sys/stat.h>  // stat, lstat, fstat
+# include <sys/wait.h>  // wait, waitpid, wait3, wait4
+# include <term.h>      // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <termios.h>   // tcsetattr, tcgetattr
 
-# define BLUE	"\033[34m"	// azul
-# define GRAY	"\033[37m"	// gris
-# define RESET	"\033[0m"
-# define SOFT_YELLOW "\033[38;5;229m"  // amarillo pastel
-# define WARM_GRAY   "\033[38;5;245m"  // gris cálido
+# define BLUE "\033[34m" // azul
+# define GRAY "\033[37m" // gris
+# define RESET "\033[0m"
+# define SOFT_YELLOW "\033[38;5;229m" // amarillo pastel
+# define WARM_GRAY "\033[38;5;245m"   // gris cálido
+
+typedef enum e_token_type
+{
+	EMPTY,
+	CMD,
+	ARG,
+	TRUNK,
+	APPEND,
+	INPUT,
+	PIPE,
+	END
+}					t_token_type;
+
+typedef struct s_cmd // Estructura para el comando parseado
+{
+	char	**arg;			// argumentos del comando
+	char	*infile;		// redirección de entrada
+	char	*outfile;		// redirección de salida
+	int		append;		// si es >> o >
+	int		is_builtin;	// si es builtin
+	struct s_cmd	*next;		// siguiente comando del pipe
+}					t_cmd;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}					t_token;
+
 #endif
