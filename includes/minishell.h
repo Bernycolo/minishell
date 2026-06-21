0348103d1a99a5fc218d7ea0fe5e26a3705c18bb
Bernardo Conejo <quinasa_05_radian@icloud.com>
2026-06-21 21:46:20 +0200

    feat: Enhance command and redirection structures for improved parsing and execution

--- includes/minishell.h
+++ includes/minishell.h
@@ -40,7 +40,7 @@
 /**
  * @brief Represents a node in the command linked list
  * 
- */
+
 typedef struct s_cmd
 {
 	char	**arg;		// argumentos del comando
@@ -50,6 +50,38 @@ typedef struct s_cmd
 	int		is_builtin;	// si es builtin
 	struct s_cmd	*next;
 }					t_cmd;
+*/
+
+
+/**
+ * @struct s_redir
+ * @brief Represents a single redirection associated with a command.
+ *
+ */
+
+typedef struct s_redir
+{
+    int             type;       /**< Redirection type */
+    char            *target;    /**< File name or heredoc delimiter */
+    struct s_redir  *next;      /**< Next redirection */
+}   t_redir;
+
+/**
+ * @struct s_cmd
+ * @brief Represents a fully parsed command ready for execution.
+ *
+ */
+typedef struct s_cmd
+{
+    char            **argv;         /**< Command + arguments */
+    int             argc;           /**< Number of arguments */
+    char            *cmd_path;      /**< Resolved executable path */
+    t_redir         *redirs;        /**< Linked list of redirections */
+    int             is_builtin;     /**< Whether the command is a builtin */
+    struct s_cmd    *next;          /**< Next command in a pipeline */
+}   t_cmd;
+
+
 
 /*					Parser							*/
 t_status			parse(const char *input, t_cmd *cmd);
